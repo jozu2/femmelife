60ed6d598ab53d73b5d2dc8b5e98d96709add177
jozu2 <webdevjosu@gmail.com>
2023-11-24 14:20:20 +0800

    remove

--- context/store.js
+++ context/store.js
@@ -1,13 +1,9 @@
 // store.js
 import { createStore, combineReducers } from "redux";
 import waterTrackerReducer from "./reducers/waterTrackerReducer";
-import navReducer from "./navSlice";
 
 const rootReducer = combineReducers({
   waterTracker: waterTrackerReducer,
-  reducer: {
-    nav: navReducer,
-  },
 });
 
 const store = createStore(rootReducer);
